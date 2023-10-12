0ce1d6f3e41789e3c6dd09283c0d7681dfa22bf8
jasminehaf <jasminenaylahafiezh@gmail.com>
2023-10-12 14:50:37 +0700

    update index.html

---
+++ main.js
@@ -0,0 +1,80 @@
+// console.log("Hello World")
+// alert("Notifikasi")
+// prompt("Pinjam dulu seratus, boleh?")
+
+// var Promnet = "Coding is Ez"
+// console.log(Promnet)
+// var Promnet = "Coding not ez"
+// console.log(Promnet)
+
+// let Promnet = "Coding is Ez"
+// console.log(Promnet)
+// let Promnet23 = "Coding not Ez"
+// console.log(Promnet23)
+
+// const Promnet = "Coding is Ez"
+// console.log(Promnet)
+// Promnet = "Coding not Ez"
+// console.log(Promnet)
+
+// let totalPoin = prompt("Masukkan poin Anda")
+//     if(totalPoin > 100){
+//         document.write("<h1>Congratulation</h1>");
+//     } 
+//     else {
+//         document.write("<h1>Thank you</h1>");
+//     }
+
+// let x = 6;
+// let y = 3;
+
+// console.log((x<10 && y>1))
+// console.log((x<10 && y<1))
+// console.log((x==5 || y==5)) 
+// console.log((x==6 || y==5)) 
+// console.log(!(x == y))
+
+// if (x<10 && y>1) {
+//     console.log("a: true");
+// } 
+// else {
+//     console.log("a: false");
+// }
+
+// if(x<10 && y<1) {
+//     console.log("b: true");
+// } 
+// else {
+//     console.log("b: false");
+// } 
+
+// if(x==5 || y==5) {
+//     console.log("c: true");
+// } 
+// else {
+//     console.log("c: false");
+// }
+
+// if(x==6 || y==5) {
+//     console.log("d: true");
+// } 
+// else {
+//     console.log("d: false");
+// }
+  
+// if(!(x == y)) {
+//     console.log("e: true");
+// } 
+// else {
+//     console.log("e: false");
+// }
+
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
+
+// button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+// })
